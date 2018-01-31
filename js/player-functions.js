150f09a8b4f809ed932ded8b153b45fd5950cecd
Soren Gravesen <S_Gravesen92@hotmail.com>
2018-01-31 21:31:15 +0100

    rearranged player variables and added placeholder for the RemoveFromInventory function

--- js/player-functions.js
+++ js/player-functions.js
@@ -10,12 +10,17 @@
 */
 
 // Player variables
-var playerName = "";
-var playerHealth = 100;
-var playerBasicDamage = 10;
+var playerName;
 var playerBonusDamage = 0; // Based on level + weapon
-var playerGold = 0;
 var playerInventory = []; // list of objects
+
+// player stats variables
+var playerGold = 0;
+var playerBasicDamage = 10;
+var playerHealth = 100;
+var playerLevel = 1;
+var playerXp = 0;
+var playerXpForNextLevel = 100;
 
 //---------------------------------//
 
@@ -23,4 +28,9 @@ var playerInventory = []; // list of objects
 var AddToInventory = function(item) {
     this.playerInventory.push(item)
     renderInventory();
+}
+
+// 2: Remove from inventory
+var RemoveFromInventory = function(item) {
+    
 }
