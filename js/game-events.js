d7fc965addd8b9b57c67810f1684ed374c6f7af7
Soren Gravesen <S_Gravesen92@hotmail.com>
2018-02-01 22:13:34 +0100

    Added default intro-text to the shop

--- js/game-events.js
+++ js/game-events.js
@@ -12,7 +12,20 @@
 
 // 1: Shop (Buy)
 var ShopBuyEvent = function() {
-    console.log("ShopBuyEvent");
+    console.log("ShopEvent fired");
+    $("#scene-outer-container").css("display", "none");
+
+    // heading
+    $("#shop-event-container .description-container h4").html("Nimba Shop");
+    // instructional text
+    $("#shop-event-container .description-container")
+    .after("<p class='text-instructions'>Select the items you want to buy. \
+    Accept by clicking Buy, or cancel by clicking Back</p>");
+
+    // hide event when clicking back
+    // set variable to visited
+    // build current scene again -- BuildScene(CurrentScene) to load new dialogue
+    // show event after build complete
 }
 
 var ShopSellEvent = function() {
