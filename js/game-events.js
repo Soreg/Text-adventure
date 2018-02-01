8d5396039ab534c20512913363647a126fb37e37
Soren Gravesen <S_Gravesen92@hotmail.com>
2018-02-01 22:51:30 +0100

    Added functionality to load items into shop

--- js/game-events.js
+++ js/game-events.js
@@ -13,14 +13,24 @@
 // 1: Shop (Buy)
 var ShopBuyEvent = function() {
     console.log("ShopEvent fired");
+
     $("#scene-outer-container").css("display", "none");
+    $("#shop-event-container").css("display", "block");
 
     // heading
     $("#shop-event-container .description-container h4").html("Nimba Shop");
     // instructional text
     $("#shop-event-container .description-container")
-    .after("<p class='text-instructions'>Select the items you want to buy. \
+    .append("<p class='text-instructions'>Select the items you want to buy. \
     Accept by clicking Buy, or cancel by clicking Back</p>");
+
+    var shopContainer = $("#shop-event-container .shop-container ul");
+    // List items in the shop along with the item stats
+    $.each(NimbaShopItems, function(i, item) {
+        shopContainer.append("<li class='shop-item'><div>" + item.name + "</div> \
+        <div>" + item.specialStat[0] + "</div>\
+        <div>" + item.value + "</div></li>");
+    });
 
     // hide event when clicking back
     // set variable to visited
